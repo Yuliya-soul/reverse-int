a1cfd822ff4be83f85ef066dd55c4f1cadd3c1d2
Your Name <darki@tut.by>
2021-03-15 00:21:04 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   let result=parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        ) 
+    return result  
 }
